e12ed07ae33d4ea229c1d16e7c818702d2dfdc7a
Sebastian Morales <sebastian.moralesd@gmail.com>
2021-01-18 10:10:28 -0800

    fix layout

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/smoralesd/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/smoralesd/keymap.c
@@ -67,67 +67,67 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_5x6(
         LAMBDA ,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
         ARROW  ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                       KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,XXXXXXX,
-        XXXXXXX,HOME_A ,HOME_S ,HOME_D ,HOME_F ,KC_G   ,                       KC_H   ,HOME_J ,HOME_K ,HOME_L ,HOME_Q,XXXXXXX,
+        XXXXXXX,HOME_A ,HOME_S ,HOME_D ,HOME_F ,KC_G   ,                       KC_H   ,HOME_J ,HOME_K ,HOME_L ,HOME_Q ,XXXXXXX,
         XXXXXXX,KC_Z   ,HOME_X ,KC_C   ,KC_V   ,KC_B   ,                       KC_N   ,KC_M   ,KC_COMM,HOME_DT,KC_SLSH,XXXXXXX,
                         XXXXXXX,XXXXXXX,                                                       XXXXXXX,XXXXXXX,
                                                 MEDR   ,NAVR   ,           NSL,FUNL   ,
                                                 XXXXXXX,MOUS   ,          NSSL,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,        XXXXXXX,XXXXXXX
+                                                XXXXXXX,XXXXXXX,       XXXXXXX,XXXXXXX
     ),
 
     [_NAVR] = LAYOUT_5x6(
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      KC_CAPS,KC_UNDO,CUT    ,COPY   ,PASTE  ,XXXXXXX,
-        XXXXXXX,KC_LGUI,KC_LALT,KC_LCTL,KC_LSFT,XXXXXXX,                      KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,XXXXXXX,XXXXXXX,
-        XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,                      KC_HOME,KC_PGDN,KC_PGUP,KC_END ,KC_INS ,XXXXXXX,
-                        XXXXXXX,XXXXXXX,                                                      XXXXXXX,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,      KC_BSPC,KC_DEL ,
-                                                XXXXXXX,XXXXXXX,       KC_ENT,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,      XXXXXXX,XXXXXXX
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                       KC_CAPS,KC_UNDO,CUT    ,COPY   ,PASTE  ,XXXXXXX,
+        XXXXXXX,KC_LGUI,KC_LALT,KC_LCTL,KC_LSFT,XXXXXXX,                       KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,                       KC_HOME,KC_PGDN,KC_PGUP,KC_END ,KC_INS ,XXXXXXX,
+                        XXXXXXX,XXXXXXX,                                                       XXXXXXX,XXXXXXX,
+                                                XXXXXXX,XXXXXXX,       KC_BSPC,KC_DEL ,
+                                                XXXXXXX,XXXXXXX,        KC_ENT,XXXXXXX,
+                                                XXXXXXX,XXXXXXX,       XXXXXXX,XXXXXXX
     ),
 
     [_MOUS] = LAYOUT_5x6(
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,KC_LGUI,KC_LALT,KC_LCTL,KC_LSFT,XXXXXXX,                      KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,XXXXXXX,XXXXXXX,
-        XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,                      KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,XXXXXXX,XXXXXXX,
-                        XXXXXXX,XXXXXXX,                                                      XXXXXXX,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,      KC_BTN3,KC_BTN2,
-                                                XXXXXXX,XXXXXXX,      KC_BTN1,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,      XXXXXXX,XXXXXXX
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,KC_LGUI,KC_LALT,KC_LCTL,KC_LSFT,XXXXXXX,                       KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,                       KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,XXXXXXX,XXXXXXX,
+                        XXXXXXX,XXXXXXX,                                                       XXXXXXX,XXXXXXX,
+                                                XXXXXXX,XXXXXXX,       KC_BTN3,KC_BTN2,
+                                                XXXXXXX,XXXXXXX,       KC_BTN1,XXXXXXX,
+                                                XXXXXXX,XXXXXXX,       XXXXXXX,XXXXXXX
     ),
 
     [_MEDR] = LAYOUT_5x6(
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,KC_LGUI,KC_LALT,KC_LCTL,KC_LSFT,XXXXXXX,                     KC_MPRV,KC_VOLD,KC_VOLU,KC_MNXT,XXXXXXX,XXXXXXX,
-        XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-                        XXXXXXX,XXXXXXX,                                                     XXXXXXX,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,     KC_MPLY,KC_MUTE,
-                                                XXXXXXX,XXXXXXX,     KC_MSTP,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,     XXXXXXX,XXXXXXX
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,KC_LGUI,KC_LALT,KC_LCTL,KC_LSFT,XXXXXXX,                       KC_MPRV,KC_VOLD,KC_VOLU,KC_MNXT,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+                        XXXXXXX,XXXXXXX,                                                       XXXXXXX,XXXXXXX,
+                                                XXXXXXX,XXXXXXX,       KC_MPLY,KC_MUTE,
+                                                XXXXXXX,XXXXXXX,       KC_MSTP,XXXXXXX,
+                                                XXXXXXX,XXXXXXX,       XXXXXXX,XXXXXXX
     ),
 
     [__NSL] = LAYOUT_5x6(
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,KC_LBRC,KC_7   ,KC_8   ,KC_9   ,KC_RBRC,                    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,KC_SCLN,KC_4   ,KC_5   ,KC_6   ,KC_EQL ,                    XXXXXXX,KC_LSFT,KC_LCTL,KC_LALT,KC_LGUI,XXXXXXX,
-        XXXXXXX,KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_BSLS,                    XXXXXXX,XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,
-                        XXXXXXX,XXXXXXX,                                                    XXXXXXX,XXXXXXX,
-                                                KC_DOT ,KC_0,       XXXXXXX,XXXXXXX,
-                                                XXXXXXX,KC_MINS,    XXXXXXX,XXXXXXX,
-                                                XXXXXXX,XXXXXXX,    XXXXXXX,XXXXXXX
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,KC_LBRC,KC_7   ,KC_8   ,KC_9   ,KC_RBRC,                      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,KC_SCLN,KC_4   ,KC_5   ,KC_6   ,KC_EQL ,                      XXXXXXX,KC_LSFT,KC_LCTL,KC_LALT,KC_LGUI,XXXXXXX,
+        XXXXXXX,KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_BSLS,                      XXXXXXX,XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,
+                        XXXXXXX,XXXXXXX,                                                      XXXXXXX,XXXXXXX,
+                                                KC_DOT ,KC_0,         XXXXXXX,XXXXXXX,
+                                                XXXXXXX,KC_MINS,      XXXXXXX,XXXXXXX,
+                                                XXXXXXX,XXXXXXX,      XXXXXXX,XXXXXXX
     ),
 
     [_NSSL] = LAYOUT_5x6(
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,KC_LCBR,KC_AMPR,KC_ASTR,KC_LPRN,KC_RCBR,                    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-        XXXXXXX,KC_COLN,KC_DLR ,KC_PERC,KC_CIRC,KC_PLUS,                    XXXXXXX,KC_LSFT,KC_LCTL,KC_LALT,KC_LGUI,XXXXXXX,
-        XXXXXXX,KC_TILD,KC_EXLM,KC_AT  ,KC_HASH,KC_PIPE,                    XXXXXXX,XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,
-                        XXXXXXX,XXXXXXX,                                                    XXXXXXX,XXXXXXX,
-                                               KC_LPRN,KC_RPRN,     XXXXXXX,XXXXXXX,
-                                               XXXXXXX,KC_UNDS,     XXXXXXX,XXXXXXX,
-                                               XXXXXXX,XXXXXXX,     XXXXXXX,XXXXXXX
+        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,KC_LCBR,KC_AMPR,KC_ASTR,KC_LPRN,KC_RCBR,                     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,KC_COLN,KC_DLR ,KC_PERC,KC_CIRC,KC_PLUS,                     XXXXXXX,KC_LSFT,KC_LCTL,KC_LALT,KC_LGUI,XXXXXXX,
+        XXXXXXX,KC_TILD,KC_EXLM,KC_AT  ,KC_HASH,KC_PIPE,                     XXXXXXX,XXXXXXX,XXXXXXX,KC_RALT,XXXXXXX,XXXXXXX,
+                        XXXXXXX,XXXXXXX,                                                     XXXXXXX,XXXXXXX,
+                                               KC_LPRN,KC_RPRN,      XXXXXXX,XXXXXXX,
+                                               XXXXXXX,KC_UNDS,      XXXXXXX,XXXXXXX,
+                                               XXXXXXX,XXXXXXX,      XXXXXXX,XXXXXXX
     ),
 
     [_FUNL] = LAYOUT_5x6(
